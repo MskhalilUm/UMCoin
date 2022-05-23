c894e94f12d87fcd4974874ab5362021ae43050f
Mohammed Khalil <mohammedkuf@gmail.com>
2022-05-23 11:32:49 +0300

    Initial Commit

---
+++ UMCoin/UMCoin.h
@@ -0,0 +1,18 @@
+//
+//  UMCoin.h
+//  UMCoin
+//
+//  Created by Umniah Developer on 23/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UMCoin.
+FOUNDATION_EXPORT double UMCoinVersionNumber;
+
+//! Project version string for UMCoin.
+FOUNDATION_EXPORT const unsigned char UMCoinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UMCoin/PublicHeader.h>
+
+
