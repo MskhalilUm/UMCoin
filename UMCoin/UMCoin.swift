e9ded7f7936703c195059e70ae510fc9ad395de0
Mohammed Khalil <mohammedkuf@gmail.com>
2022-05-23 12:05:48 +0300

    Added test cases

---
+++ UMCoin/UMCoin.swift
@@ -0,0 +1,23 @@
+//
+//  UMCoin.swift
+//  UMCoin
+//
+//  Created by Umniah Developer on 23/05/2022.
+//
+
+import Foundation
+
+
+public final class UMCoin {
+
+    let name = "UMCoin"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
